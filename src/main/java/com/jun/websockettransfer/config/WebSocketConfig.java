291c57a5869d71bd425257fb0a0e5cfdbeca6c0e
xzj <2837665615@qq.com>
2025-12-05 22:47:34 +0800

    websocket中转程序

--- src/main/java/com/jun/websockettransfer/config/WebSocketConfig.java
+++ src/main/java/com/jun/websockettransfer/config/WebSocketConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
  * WebSocket核心配置，注册用户端连接端点
